f817763b9c4f01a64754e7c8fd7ceddb27c35651
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-05-15 13:36:17 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,489 @@
+// // // // // // // data type
+
+// // // // // // //string
+
+// // // // // // // is an alphanumeric datatypes placed within quotation mark
+
+// // // // // // // "1234"
+// // // // // // // "helo123@;"
+
+// // // // // // // 'hello'
+// // // // // // // '1234'
+
+// // // // // // // console.log(typeof('hello'))
+
+// // // // // // // // number is a numerical value\
+
+// // // // // // // 12
+// // // // // // // 10
+// // // // // // // 76.5
+// // // // // // // 7657
+
+// // // // // // // console.log(45423567890876453221457689043256789 *2)
+// // // // // // // console.log(45423567890876453221457689043256789n *2n)
+
+// // // // // // //boolean is a true or false value
+
+// // // // // // // let myVariable = "get out"
+// // // // // // // let car = null
+
+// // // // // // // car = 'red'
+// // // // // // // console.log(20 == 500)
+
+// // // // // // // console.log(myVariable)
+// // // // // // // console.log(car)
+
+// // // // // // // Object
+
+// // // // // // x = 20
+
+// // // // // // x = true
+
+// // // // // // let y = 300
+
+// // // // // // y= 500
+
+// // // // // // console.log(x)
+// // // // // // console.log(y)
+
+// // // // // // const z = 7000
+
+// // // // // // z = 600
+// // // // // // console.log(z)
+
+// // // // // // DataTypes
+
+// // // // // // when using values in js
+// // // // // // fixed values
+// // // // // 10
+// // // // // "ert"
+// // // // // true
+
+// // // // // //variable
+
+// // // // // // number - represents numeric values
+
+// // // // // 43564
+// // // // // // string - alphanumericsymbolic value wrapped within quoutation marks
+// // // // // "Jumoke" ;"20";"@gkjds"; 'Samu'
+// // // // // //object - represents a container for multiple values
+// // // // //     // array
+// // // // //     // object
+// // // // //     // date
+// // // // //     //map
+// // // // //     // set
+// // // // // //boolean - represents true or false
+// // // // // // BigInt - represent a big Integer(number)
+// // // // // 123456789012345678987654321n
+
+// // // // // // null
+
+// // // // // let test = null
+
+// // // // // // undefined
+
+// // // // // //Variables
+
+// // // // // // automatically
+
+// // // // // age = 307
+// // // // // console.log(age)
+
+// // // // // //variable declared automatically are mutable
+
+// // // // // age = 26
+
+// // // // // // console.log(age)
+
+// // // // // // // let keyword
+
+// // // // // // let person = "Jumoke"
+
+// // // // // // console.log(person)
+
+// // // // // // person = "Bidemi"
+
+// // // // // // console.log(person)
+
+// // // // // // // const keyword
+
+// // // // // // const stage = "js Student"
+
+// // // // // // console.log(stage)
+
+// // // // // // Operators
+
+// // // // // //  = - assignment operator - assigns a value to  a variable
+// // // // // // + - addition operator
+// // // // // // -
+// // // // // // /
+// // // // // // *
+// // // // // //  % - modulus - returns remainder of division operation
+
+// // // // // // console.log(2 + 2)
+// // // // // // console.log(2 - 2)
+// // // // // // console.log(2 / 2)
+// // // // // // console.log(2 * 2)
+// // // // // // console.log(1000001 % 2)
+
+// // // // // // > - greater than
+// // // // // // < - less than
+// // // // // // >= - greater or equal
+// // // // // // <= less or equal
+// // // // // // == equal
+// // // // // // === strict equal
+// // // // // // !=  inequal
+// // // // // //  !== strict inequal
+
+// // // // // // console.log(10 > 20)
+// // // // // // console.log(10 < 20)
+// // // // // // console.log(10 >= 20)
+// // // // // // console.log(20 <= 20)
+// // // // // // console.log(20 == "20")
+// // // // // // console.log(20 === "20")
+// // // // // // console.log(20 != "20")
+// // // // // // console.log(20 !== 20)
+
+// // // // // // && - and
+
+// // // // // console.log(10 > 5 && 10 > 2)
+
+// // // // // // || - or
+
+// // // // // console.log(10 > 5 || 10 > 20)
+
+// // // // // Great! Here's an extended **JavaScript Basics Worksheet** with **20 questions** (no objects), covering data types, variables, and operators:
+
+// // // // // ---
+
+// // // // // ## 📝 **JavaScript Basics Worksheet (20 Questions)**
+
+// // // // // ### *Topic: Data Types, Variables, and Operators*
+
+// // // // // ---
+
+// // // // // ### **Section A: Data Types**
+
+// // // // // **1.** What is the data type of `"Hello123"`?
+
+// // // // // ```js
+// // // // // console.log(typeof "Hello123");
+// // // // // ```
+
+// // // // // **2.** What is the data type of `999999999999999999999n`?
+
+// // // // // BigInt
+
+// // // // // **3.** What is the data type of `null`?
+
+// // // // // null
+
+// // // // // **4.** What is the data type of `true`?
+
+// // // // // booolean
+
+// // // // // **5.** What will `typeof undefined` return?
+// // // // console.log(typeof undefined)
+
+// // // // // ---
+
+// // // // // ### **Section B: Type Comparisons**
+
+// // // // // **6.** Compare the following and write the output:
+
+// // // // // ```js
+// // // // // console.log(20 == "20");
+
+// // // // // true
+
+// // // // // console.log(20 === "20");
+
+// // // // //false
+// // // // // ```
+
+// // // // // **7.** What's the difference between `==` and `===`?
+
+// // // // //
+
+// // // // // **8.** What will this return?
+
+// // // // // ```js
+// // // // console.log(typeof (5 + "5"));
+
+// // // // // implicit type conversion
+
+// // // // // ```
+
+// // // // // ---
+
+// // // // // ### **Section C: Variables**
+
+// // // // // **9.** What is wrong with this code?
+
+// // // // // ```js
+// // // // // const age = 25;
+// // // // // age = 30;
+// // // // // ```
+
+// // // // // **10.** Fix the error above using the correct keyword.
+
+// // // // // let age = 25;
+// // // // // age = 30;
+
+// // // // // **11.** Declare a variable `score` and assign it the value `100`. Then reassign it to `200`.
+
+// // // // // let score = 100
+// // // // // score = 200
+
+// // // // // ---
+
+// // // // // ### **Section D: Boolean and Comparison Operators**
+
+// // // // // **12.** What does this evaluate to?
+
+// // // // // ```js
+// // // // // console.log(10 > 5 && 5 > 3);
+
+// // // // // ```
+
+// // // // // **13.** What does this evaluate to?
+
+// // // // // ```js
+// // // // // console.log(10 < 5 || 5 > 3);
+// // // // // ```
+
+// // // // // **14.** Use `!==` to check if `5` is not strictly equal to `"5"`.
+
+// // // // // console.log(5 !== "5")
+
+// // // // // **15.** Write a comparison that checks if `40` is less than or equal to `40`.
+
+// // // // // console.log(40 <= 40)
+
+// // // // // ---
+
+// // // // // ### **Section E: Arithmetic Operators**
+
+// // // // // **16.** What is the remainder of:
+
+// // // // // ```js
+// // // // // console.log(10 % 3);
+// // // // // ```
+
+// // // // // **17.** Multiply `12345678901234567890n * 2n` using BigInt.
+
+// // // // // console.log(12345678901234567890n * 2n)
+
+// // // // // **18.** Divide `50 / 5` and print the result.
+
+// // // // // console.log(50/5)
+
+// // // // // **19.** Subtract `100 - 75` and store the result in a variable called `difference`.
+
+// // // // // let difference = 100 - 75
+
+// // // // // console.log(difference)
+
+// // // // // **20.** Add `25 + "5"` and explain the result.
+
+// // // // console.log(25 + "5")
+// // // // // ---
+
+// // // // // ✍️ Let me know if you’d like an **answer key**, or want this as a **PDF/Word document**!
+
+// // // // Type conversion
+
+// // // // type conversion is the process of converting values from one datatype to another
+
+// // // // implicit type conversions
+
+// // // // console.log(typeof (20 + "20"), 20 + "20")
+// // // // console.log(typeof (20 * "20"), 20 * "20")
+// // // // console.log(typeof (20 / "20"), 20 / "20")
+// // // // console.log(typeof (20 - "20"), 20 - "20")
+
+
+
+
+// // // // explicit type conversion
+
+// // // // to string
+
+// // // let test = 20;
+// // // console.log(test);
+// // // let numtostr = String(test)
+// // // console.log(numtostr)
+
+
+// // // //to boolean
+
+// // // console.log(Boolean("9"))
+// // // console.log(Boolean(0))
+
+
+// // // //to number
+
+// // // console.log(Number(true))
+// // // console.log(Number(false))
+
+
+
+// // // if...else
+
+// // let a = 200
+// // let c = 70000000000
+
+// // if (a > c){
+// //    console.log("a is greater than c")
+// // }
+// // else{
+// //     console.log("a is not greater than c")
+// // }
+
+
+// // Assignment
+
+
+// //  let myName = "Jumoke"
+// //  console.log(myName)
+
+// //  let age = 45
+
+// //  let course = "web-dev"
+
+// //  console.log(age,course)
+
+// // const number = 20
+// // const number2 = 5798
+
+// // console.log(number + number2)
+
+
+// // let celcius = 100
+
+// // let fahrenheit = (celcius * 9/5) + 32
+
+// // console.log(fahrenheit)
+
+
+
+// // value1 = 10
+// // value2 = 20
+
+// // value1 = value2 + (value2 = value1,0)
+
+// // console.log(value1,value2)
+
+
+
+// // console.log(10 + (100 * 5,0))
+
+
+
+// //for  loop
+// // while loop
+// //do...while loop
+// //for..in loop
+
+
+// //for loop
+
+
+// // for(i = 0;i < 100;i++){
+// //     console.log(i)
+// // }
+
+// // for(i= 100; i > 0; i--){
+// //     console.log(i)
+// // }
+
+
+// // let i = 0
+
+// // // while(i < 100){
+// // // console.log(i)
+// // //     i++
+// // // }
+
+
+// // do{
+// //    console.log(i)
+// //     i++
+// // }
+// // while(i < 100)
+
+
+// // for(a=0;a<=10;a++){
+// //     console.log(a)
+// // }
+
+// // for (a = 1; a<=20;a++){
+// //     if(a%2 === 0){
+// //         console.log(a)
+// //     }
+// // }
+
+// // let sum = 0;
+
+// // for( a = 0;a<= 100;a++){
+// //     sum = sum + a
+// //     console.log(sum)
+// // }
+// // let number = 7
+
+// // for(a = 1; a<=12;a++){
+// //     console.log(`7 X ${a}  = ${number * a}`)
+// // }
+
+// // let string = "Javascript"
+// // let reversed = ""
+// // for(a = string.length-1;a >= 0 ; a--){
+// //     reversed += string[a]
+// //     console.log(string[a],a)
+// // }
+// // console.log(reversed)
+// let str = "programming is fun "
+// let count = 0
+
+// for(a= 0; a < str.length ;a++){
+//     if(str[a] == 'a'|| str[a] == 'e'|| str[a] == 'i' || str[a] == 'o'|| str[a] == 'u'){
+//         count++
+//     }
+// }
+
+// console.log(count)
+
+
+// // functions
+
+// // function declaration
+
+// function runAlert(){
+//     alert("Good morning,Nigeria")
+// }
+
+
+// runAlert()
+
+// // function expression
+
+// const myFunc = function(){
+//     alert("God bless Nigeria")
+// }
+// myFunc()
+
+// //arrow function
+
+// const myArrow = () =>{
+//     alert("hello")
+// }
+
+// myArrow()
+
+
+
+function test(a){
+    console.log(a)
+}
+
+test(23)
